48d8019f015bb1f1b3654ae394f9f1c6ebfdc461
Wpetula <wyattpeterpetula@gmail.com>
2025-03-01 15:02:21 -0500

    Sim Infra
    Added simulation infrastructure

--- ACM_ALWorkshop/Assets/Scripts/DynamicParticle.cs
+++ ACM_ALWorkshop/Assets/Scripts/DynamicParticle.cs
@@ -1,16 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DynamicParticle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float ship_force_add = 1.3f;
+    public float relationshipForce;
+    public string identifier;
+
+    public static List<DynamicParticle> dynamicBodiesList;
+    Rules rules;
+
+    public Rigidbody2D rb2D;
     void Start()
     {
-        
+        identifier = gameObject.tag;
+        rules = GameObject.Find("Rules").GetComponent<Rules>();
+    }
+    
+    void FixedUpdate()
+    {
+        if(!Input.GetKey(KeyCode.Space)){
+            DynamicParticle[] dynamicBodiesArray = FindObjectsByType<DynamicParticle>(FindObjectsSortMode.None);
+
+            // Yes, this code is unoptimized. No, I don't have time to fix it.
+            foreach (DynamicParticle DynamicParticle in dynamicBodiesArray)
+            {
+                if (DynamicParticle == this) continue;
+                
+                if(identifier.Equals("Blue") && DynamicParticle.tag.Equals("Blue"))
+                    Attract(DynamicParticle, rules.bTB);
+
+                else if(identifier.Equals("Yellow") && DynamicParticle.tag.Equals("Yellow"))
+                    Attract(DynamicParticle, rules.yTY);
+                
+                else if(identifier.Equals("Orange") && DynamicParticle.tag.Equals("Orange"))
+                    Attract(DynamicParticle, rules.oTO);
+
+                else if(identifier.Equals("Blue") && DynamicParticle.tag.Equals("Yellow"))
+                    Attract(DynamicParticle, rules.bTY);
+
+                else if(identifier.Equals("Blue") && DynamicParticle.tag.Equals("Orange"))
+                    Attract(DynamicParticle, rules.bTO);
+
+                else if(identifier.Equals("Yellow") && DynamicParticle.tag.Equals("Blue"))
+                    Attract(DynamicParticle, rules.yTB);
+
+                else if(identifier.Equals("Yellow") && DynamicParticle.tag.Equals("Orange"))
+                    Attract(DynamicParticle, rules.yTO);
+
+                else if(identifier.Equals("Orange") && DynamicParticle.tag.Equals("Yellow"))
+                    Attract(DynamicParticle, rules.oTY);
+                
+                else Attract(DynamicParticle, rules.oTB);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attract(DynamicParticle objectToAttract, float forceStrength)
     {
-        
+        if(forceStrength != 0){
+            Rigidbody2D rb2DToAttract = objectToAttract.rb2D;
+            Vector2 direction = (rb2D.position - rb2DToAttract.position).normalized;
+            Vector2 force = direction * forceStrength;
+            rb2DToAttract.AddForce(force);
+        }
     }
 }
---
+++ ACM_ALWorkshop/Assets/Scripts/Rules.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Rules : MonoBehaviour
+{
+    public float bTY;
+    public float bTO;
+    public float yTB;
+    public float yTO;
+    public float oTB;
+    public float oTY;
+    public float bTB;
+    public float yTY;
+    public float oTO;
+}
